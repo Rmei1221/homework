b41127928b26566916d5a1ec757cf08015ad5787
Rmei1221 <sshes1221@gmail.com>
2016-04-11 00:00:41 +0800

    Added files via upload

---
+++ karewu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>∥   BEAUTY WORLD   ∥</title>
+	<link href="trytry.css" rel="stylesheet" type="text/css" />
+	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+	<div id="Wrapper">
+		<div id="Header">
+			<CENTER><a href="http://127.0.0.1/trytry.php"><img src="https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12718104_1125590937463023_6895429103174427201_n.jpg?oh=915282fbe41d0838ba19dee3c542ddbd&oe=57787379" width="" height="" border="0" ></a></CENTER>
+			<div class="MENU">
+				<ul>
+  				<li><a href="mid1.php" title="首頁">│    首頁    │</a></li>
+  				<li><a href="trytry.php" title="分享區">│  校草  │</a></li>
+  				<li><a href="flower.php" title="活動">│  校花  │</a></li>
+  				<li><a href="#" title="排行榜">│  排行榜  │</a></li>
+  				<li><a href="#" title="留言板">│  討論區  │</a></li>
+  				<li><a href="login.php" title="留言板">│  Login  │</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div id="Content">
+		<div id="BOX">
+        <p>&nbsp;</p>
+        <h3 CLASS="AA">∥    世新帥哥    ∥- 吳評楷簡介</h3>
+		<CENTER><img src="https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/12472557_1125707157451401_6707806507822387421_n.jpg?oh=49818536874f36e5b274f880052930f8&oe=578F8F11" width="20%" height="20%" align="center"></CENTER><center>
+		</br>
+		<HR>
+		<h4>本名 : 吳評楷</h4> 
+		<h4>別名   :   Kare</h4> 
+		<h4>姓別      :      男</h4> 
+		<h4>學校      :      世新大學</h4>
+		<h4>系級/別      :      108 ∥ 廣播電影學系</h4>  
+
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="http://127.0.0.1/trytry.php"><img src="http://www.unixigroup.com/img/goback2.png" width="7%" height="7%" border="0" align="center"></a>
+		</center>
+
+
+<div style='clear:both;'></div>
+
+
+<div id="Footer">
+<p id="Footer"></p>
+<HR>
+<address><footer><CENTER>copyright 2016 by BeautyWorld</br>Designed by 高雄大學資管系</CENTER></footer></address>
+
+</div>
+
+</div>
+
+</body>
+</html>
